5c4c041d768d9ee8a45a811f21a07e5c16d72484
Naufal Hilmi <145202912+naufalhilmi-20@users.noreply.github.com>
2023-11-12 18:25:30 +0000

    Update 11 files

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,7 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
+  github: "https://github.com/naufalhilmi-20",
+  linkedin: "https://www.linkedin.com/in/naufal-hilmi-55742a15a/",
   medium: "https://ladvace.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
+  discord: "https://discordapp.com/users/265177916348956672",
+  instagram: "https://www.instagram.com/naufal_hilmi"
 };
